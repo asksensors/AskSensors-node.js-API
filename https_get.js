f7a1535be839348b46095e74d5a6585d1d694f74
AskSensors <42293557+asksensors@users.noreply.github.com>
2019-05-05 05:54:22 +0000

    Update https_get.js

--- https_get.js
+++ https_get.js
@@ -1,6 +1,6 @@
 /*
- * Automated data sending using https
- * Description: This example code sends dummy data to askSensors IoT platfom (https://asksensors.com) over HTTPS GET Requests.
+ * AskSensors node.js API
+ * Description: This example code sends random data to askSensors IoT platfom (https://asksensors.com) over HTTPS GET Requests.
  *  Author: https://asksensors.com, 2018
  *  github: https://github.com/asksensors
  */
@@ -9,16 +9,17 @@
 var https = require('https');
 const request = require('request');
 // Configuration
-var host = 'https://asksensors.com';		// server host
+var host = 'https://api.asksensors.com';		// server host
 var ApiKeyIn = '...................'; // TODO: fill your sensor Api Key In given by https://asksensors.com
-var module1 = 0;				// module 
-var timerInterval = 20000;		// timer interval
+var timerInterval = 20000;	// TODO: adjust timerInterval (in ms)
 // Function declaration: send data
 function send_data(){
-	var url = host + '/api.asksensors/write/';	
+	var url = host + '/write/';	
 		url+= ApiKeyIn;
 		url+= '?module1='
-		url+= 100*Math.random();//module1++;
+		url+= 100*Math.random(); // random data in module 1
+		url+= '&module2='
+		url+= 100*Math.random(); // random data in module 2
 	console.log('Data sent to:' + url);
 	request(url, { json: true }, (err, res, body) => {
 	  if (err) { return console.log(err); }
